aafdfb0b94cecd4a55012a2edd7f5bff03465d83
sevans924 <sevans924@gmail.com>
2021-08-16 11:42:00 -0400

    feat: broken tutorial

--- family-reunion-pwa/next.config.js
+++ family-reunion-pwa/next.config.js
@@ -1,3 +1,9 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
+})
